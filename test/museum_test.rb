932845e0b0c4e678a06e6e30f79411a31f988c60
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-02-19 09:35:13 -0700

    Add can_add_exhibits test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -2,6 +2,7 @@ require "pry"
 require "minitest/autorun"
 require "mocha/minitest"
 require "./lib/museum"
+require "./lib/exhibit"
 
 class MuseumTest < Minitest::Test
 
@@ -11,9 +12,24 @@ class MuseumTest < Minitest::Test
 
   def test_it_has_attributes
     dmns = Museum.new("Denver Museum of Nature and Science")
-    
+
     assert_equal "Denver Museum of Nature and Science", dmns.name
     assert_equal [], dmns.exhibits
+  end
+
+  def test_it_can_add_exhibits
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    assert_equal [], dmns.exhibits
+
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    assert_equal [gems_and_minerals, dead_sea_scrolls, imax], dmns.exhibits
   end
 
 end
