16cf51b3478ee9d5092135d3a1658b96787fa2e6
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-02-19 09:23:35 -0700

    Add can_add_interest test

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -16,30 +16,14 @@ class PatronTest < Minitest::Test
     assert_equal [], patron_1.interests
   end
 
+  def test_it_can_add_interest
+    patron_1 = Patron.new("Bob", 20)
+
+    assert_equal [], patron_1.interests
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], patron_1.interests
+  end
+
 end
-# pry(main)> require './lib/exhibit'
-# # => true
-#
-# pry(main)> require './lib/patron'
-# # => true
-
-
-#
-# pry(main)> patron_1 = Patron.new("Bob", 20)
-# # => #<Patron:0x00007fcb13b5c7d8...>
-#
-# pry(main)> patron_1.name
-# # => "Bob"
-#
-# pry(main)> patron_1.spending_money
-# # => 20
-#
-# pry(main)> patron_1.interests
-# # => []
-#
-# pry(main)> patron_1.add_interest("Dead Sea Scrolls")
-#
-# pry(main)> patron_1.add_interest("Gems and Minerals")
-#
-# pry(main)> patron_1.interests
-# # => ["Dead Sea Scrolls", "Gems and Minerals"]
