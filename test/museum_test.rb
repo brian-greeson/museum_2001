6e4d5ef3a56f72e72c073b3a41e9e58a10cdfa9b
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-02-19 10:05:52 -0700

    Add list_patrons_by_exhibit_interest test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -70,4 +70,33 @@ class MuseumTest < Minitest::Test
 
   end
 
+  def test_it_can_list_patrons_by_exhibit_interest
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    assert_equal [], dmns.patrons
+
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+
+    assert_equal ({
+                  gems_and_minerals => [patron_1],
+                  dead_sea_scrolls => [patron_1, patron_2, patron_3],
+                  imax => []
+                  }), dmns.patrons_by_exhibit_interest
+  end
+
 end
